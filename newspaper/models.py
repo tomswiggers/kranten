1b157d660abdd6cfb4e10230919288437de2c068
Tom Swiggers <swiggers.tom@gmail.com>
2012-09-07 14:02:32 +0200

    models update

--- newspaper/models.py
+++ newspaper/models.py
@@ -1,10 +1,7 @@
 from django.db import models
 
-class MyModel(models.Model):
-  field = models.CharField(max_length=255)
-  
-  def __unicode__(self):
-    return self.field
+import datetime
+from datetime import date
 
 class Client(models.Model):
   name = models.CharField('Naam', max_length=255)
@@ -16,17 +13,40 @@ class Client(models.Model):
   city = models.CharField('Gemeente', max_length=255)
   round_nbr = models.IntegerField('Ronde')
   order = models.IntegerField('Volgorde in ronde')
+  delivery_begindate = models.DateField()
+  delivery_enddate = models.DateField()
 
 class Item(models.Model):
   name = models.CharField('Naam', max_length=255)
   description = models.CharField('Beschrijving', max_length=255)
+  days = models.IntegerField()
 
+  def isDeliveryDay(self, date, days):
+    day = int(date.weekday()) + 1
+    check = pow(2, day)
+
+    if days & check > 0:
+      return True
+    else:
+      return False
+ 
 class Price(models.Model):
   begindate = models.CharField(max_length=255)
   enddate = models.CharField(max_length=255)
   item_id = models.IntegerField('Artikel')
 
 class Holiday(models.Model):
-  begindate = models.CharField(max_length=255)
-  enddate = models.CharField(max_length=255)
-  client = models.CharField(max_length=255)
+  begindate = models.DateField()
+  enddate = models.DateField()
+  client_id = models.IntegerField()
+ 
+class BankHoliday(models.Model):
+  entrydate = models.DateField()
+
+class Delivery(models.Model):
+  client_id = models.IntegerField()
+  item_id = models.IntegerField()
+  days = models.IntegerField()
+  begindate = models.DateField()
+  enddate = models.DateField()
+ 
